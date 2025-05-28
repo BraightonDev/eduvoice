fa9877fcf7bc5dfdf0b2a820d57051ae3862deb4
Robert-Otiniano <robertalexis2016@gmail.com>
2025-05-27 23:46:43 -0500

    Realizacion de pagina1 con logica y diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import LetrasAudio from "./components/LetrasAudio";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Pagina1 from './paginas/pagina1/pagina1';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-center mt-6">Prueba de Audios</h1>
-      <LetrasAudio />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Pagina1 />} />
+        {/* Placeholder para futuras páginas */}
+        <Route path="/pagina2" element={<div>Página 2</div>} />
+        <Route path="/pagina3" element={<div>Página 3</div>} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+export default App;
