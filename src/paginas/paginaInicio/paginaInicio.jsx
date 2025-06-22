d04ec2510fb9f68551cee92bc0603091e3b17e32
freddy09t <freddytn09@gmail.com>
2025-06-22 17:28:27 -0500

    todo por una P en vez de p

--- src/paginas/paginaInicio/paginaInicio.jsx
+++ src/paginas/paginaInicio/paginaInicio.jsx
@@ -1,25 +1,20 @@
-// src/paginas/PaginaInicio.jsx
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./paginaInicio.css"; // Usa el mismo archivo de estilos
+import "./paginaInicio.css"; 
 
 const PaginaInicio = () => {
   const navigate = useNavigate();
-
   const iniciarApp = () => {
     setTimeout(() => {
       window.audio = new Audio("/audios/indicaciones/indicacion 0.mp3");
       window.audio.play().catch((err) => {
         console.warn("No se pudo reproducir el audio:", err);
       });
-
-      // Redirige después de 1.5s (puedes ajustarlo si el audio es más largo)
       setTimeout(() => {
         navigate("/pagina0");
       }, 1500);
-    }); // 1 segundo de demora antes de reproducir
+    });
   };
-
   return (
     <div className="pantalla-bienvenida">
       <h1 className="titulo-pagina0">Bienvenido a EDUVOICE</h1>
@@ -29,5 +24,4 @@ const PaginaInicio = () => {
     </div>
   );
 };
-
 export default PaginaInicio;
